e8d22308f6db11842506f55371fee8451e84cd2a
saikripaarya <saikripa.arya@gmail.com>
2020-04-28 17:56:39 +0100

    sumjava committed

---
+++ sumjava.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class sumjava
+{
+	public static void main(String args[])
+	{
+                System.out.println("Enter 2 numbers for sum");
+                Scanner readme = new Scanner(System.in);
+                double n1, n2, n3;
+                n1 = readme.nextDouble();
+                n2 = readme.nextDouble();
+                n3 = n1 + n2;
+                System.out.println("Total = " + n3);
+	}
+}
+		
